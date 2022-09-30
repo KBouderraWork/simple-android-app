fa8e4eb7c98123f2ac509991c4a80fbe0e138016
khalil bouderra <bouderrakhalil@outlook.com>
2022-09-30 11:59:06 +0100

    change toast message

--- app/src/main/java/com/example/simple_android_app/MainActivity.kt
+++ app/src/main/java/com/example/simple_android_app/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "hi", Toast.LENGTH_SHORT).show()
     }
 }
