cc1cd313573c4228f8c7eaeaf0d81b3f7200d6f3
khalil bouderra <bouderrakhalil@outlook.com>
2022-10-01 12:02:07 +0100

    change timout

--- app/src/main/java/com/example/simple_android_app/MainActivity.kt
+++ app/src/main/java/com/example/simple_android_app/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(this, "Some", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Some", Toast.LENGTH_LONG).show()
     }
 }
